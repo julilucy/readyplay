0d939b612fec40750cbc649cd8e2fca3dc0e9996
Júlia Lucena <julialucena4@gmail.com>
2023-11-30 19:36:11 -0300

    Mais uma modificação

--- createdb.sql
+++ createdb.sql
@@ -1,16 +1,13 @@
-
-CREATE DATABASE readyplay;
-
 USE readyplay;
 
 CREATE TABLE usuarios (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    nomeusuario VARCHAR(100) NOT NULL,
+    nome_usuario VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     telefone VARCHAR(15) NOT NULL,
     genero VARCHAR(15) NOT NULL,
     data_nascimento DATE NOT NULL,
-    senha VARCHAR(50) NOT NULL,
+    senha VARCHAR(50) NOT NULL
 
 );
--- form.php
+++ form.php
@@ -4,39 +4,15 @@ $usuario = "root";
 $senha = "root";
 $banco = "readyplay";
 
-$conn = new mysqli($host, $usuario, $senha, $banco);
+/* $conn = new mysqli($host, $usuario, $senha, $banco);
 
 if ($conn->connect_error) {
     die("Falha na conexão com o banco de dados: " . $conn->connect_error);
-}
+} */
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["nome"];
-    $nomeusuario = $_POST["nomeusuario"];
-    $email = $_POST["email"];
-    $telefone = $_POST["telefone"];
-    $genero = $_POST["genero"];
-    $data_nascimento = $_POST["data_nascimento"];
-    $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);
-
-    $sql = "INSERT INTO usuarios (nome, nomeusuario, email, telefone, genero, data_nascimento, senha) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    
-    if (!$stmt) {
-        die("Erro na preparação da instrução SQL: " . $conn->error);
-    }
-
-    $stmt->bind_param("sssssss", $nome, $nomeusuario, $email, $telefone, $genero, $data_nascimento, $senha);
-
-    if ($stmt->execute()) {
-        echo "Cadastro realizado com sucesso!";
-    } else {
-        echo "Erro ao cadastrar usuário: " . $stmt->error;
-        echo "SQL executada: " . $sql;
-    }
-    
-
-    $stmt->close();
-    $conn->close();
+try{
+    $conn = new PDO("mysql:dbname=".$banco.";host".$host, $usuario, $senha);
+} catch(PDOException $err){
+    throw new Exception("Falha na conexão com o banco de dados: " . $err);
 }
 ?>
